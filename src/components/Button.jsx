0f090d135ce42bef3f978c06c6b0a7bf7ac418eb
manobalan-08 <manobalan.m2023cse@sece.ac.in>
2026-01-22 13:00:15 +0530

    Create My brand new Three.js Portfolio using React-three fiber , EmailJs , GSAP

---
+++ src/components/Button.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Button = ({text, className, id}) => {
+  return (
+    <a 
+     onClick={(e) =>{
+      e.preventDefault();
+
+      const target=document.getElementById('counter')
+
+      if(target && id){
+        const offset= window.innerHeight* 0.15;
+
+        const top= target.getBoundingClientRect().top + window.scrollY
+        -offset;
+
+        window.scrollTo({top , behavior: 'smooth'})
+      }
+     }}
+    
+    className={`${className ?? ""} cta-wrapper group`}>
+      <div className='cta-button'>
+        <div className='bg-circle'></div>
+        <p className='text'>{text}</p>
+        <div className='arrow-wrapper'>
+          <img src="/images/arrow-down.svg" alt="arrow" />
+        </div>
+      </div>
+    </a>
+  )
+}
+
+export default Button
