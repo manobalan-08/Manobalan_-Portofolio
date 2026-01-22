52d084ecc6e255021cf4dd485f03f8adeb000fd8
manobalan-08 <manobalan.m2023cse@sece.ac.in>
2026-01-22 14:36:25 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,25 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-      tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'three': ['three'],
+          'react-three': ['@react-three/fiber', '@react-three/drei', '@react-three/postprocessing'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+    commonjsOptions: {
+      transformMixedEsm: true,
+    }
+  },
+  optimizeDeps: {
+    include: ['three', '@react-three/fiber', '@react-three/drei', '@react-three/postprocessing'],
+  }
 })
